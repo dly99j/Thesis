bba757a2ee6862843572af2e7a27d981fd40590a
htamas <h.tomi2001@gmail.com>
2024-05-29 15:23:01 +0200

    hopefully done?

--- thesis_space_shooter/src/main.cpp
+++ thesis_space_shooter/src/main.cpp
@@ -1,16 +1,6 @@
 #include "../incl/game.hpp"
 
-#ifdef __clang__
-const char* comp = "Clang";
-#elif defined(__GNUG__)
-const char* comp = "GCC";
-#else
-const char* comp = "Unknown";
-#endif
-
 int main() {
-    std::cout << "#define __cplusplus: " << __cplusplus << std::endl;
-    std::cout << comp << std::endl;
     spsh::game gme;
     gme.run();
 }
